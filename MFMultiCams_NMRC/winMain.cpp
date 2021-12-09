62dcd4b8d01efb4ecf2e0d0ccf707297c78de3e2
LRB411-Win10 <yll0620@gmail.com>
2021-12-09 12:58:21 -0600

    added EnumerateDevices

--- MFMultiCams_NMRC/winMain.cpp
+++ MFMultiCams_NMRC/winMain.cpp
@@ -29,6 +29,38 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
 void    OnInitDialog(HWND hDlg);
 void    OnCloseDialog();
 
+HRESULT EnumerateDevices(IMFActivate** m_ppDevices, UINT32 m_cDevices)
+{
+    HRESULT hr = S_OK;
+    IMFAttributes* pAttributes = NULL;
+
+
+    // Initialize an attribute store. We will use this to 
+    // specify the enumeration parameters.
+
+    hr = MFCreateAttributes(&pAttributes, 1);
+
+    // Ask for source type = video capture devices
+    if (SUCCEEDED(hr))
+    {
+        hr = pAttributes->SetGUID(
+            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
+            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
+        );
+    }
+
+    // Enumerate devices.
+    if (SUCCEEDED(hr))
+    {
+        hr = MFEnumDeviceSources(pAttributes, &m_ppDevices, &m_cDevices);
+    }
+
+    SafeRelease(&pAttributes);
+
+    return hr;
+}
+
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -85,6 +117,11 @@ void OnInitDialog(HWND hDlg)
     HRESULT hr = S_OK;
 
     HWND hEdit = GetDlgItem(hDlg, IDC_OUTPUT_FILE);
+    SetWindowText(hEdit, TEXT("capture.mp4"));
+
+    IMFActivate** m_ppDevices = NULL;
+    UINT32 m_cDevices = 0;
+    EnumerateDevices(m_ppDevices, m_cDevices);
 }
 
 
@@ -107,3 +144,6 @@ void OnCloseDialog()
 // Enumerates the video capture devices and populates the list of device
 // names in the dialog UI.
 //-----------------------------------------------------------------------------
+
+
+
